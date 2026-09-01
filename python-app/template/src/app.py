b8e933dc14b0da0e52f8071ff181d02f2a5334e9
job.gwese <jbgwese@gmail.com>
2026-09-01 07:19:28 +0200

    use new organisation

--- python-app/template/src/app.py
+++ python-app/template/src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
      'time':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
      'hostname':socket.gethostname(),
-        'message': 'happy to be here xx',
+        'message': 'happy to be here ',
         'env': '${{values.app_env}}',
         'app_name': '${{values.app_name}}'
      })
